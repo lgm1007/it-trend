1f7760894fce953a9da966ba59acf76e50d37a99
lgm1007 <flxl654@gmail.com>
2023-04-07 17:53:22 +0900

    [New] 개발환경 초기 세팅

---
+++ src/main/kotlin/com/lgmpjt/ittrendbackend/ItTrendBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.lgmpjt.ittrendbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItTrendBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<ItTrendBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/lgmpjt/ittrendbackend/ItTrendBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lgmpjt.ittrendbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItTrendBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
